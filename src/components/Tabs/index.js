e2333846be075d4e233f37b2c3e219684bd6c723
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-05-24 12:02:45 -0300

    added tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import { Container, TabsContainer, TabItem, TabText } from './styles';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="user-plus" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="message-square" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-up" size={24} color="#fff" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-down" size={24} color="#fff" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear Cartao</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
