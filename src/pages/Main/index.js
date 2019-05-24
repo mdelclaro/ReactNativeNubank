0d2f672551fb46062d304a6417292016799688f6
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-05-24 12:33:16 -0300

    added main card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="credit-card" size={28} color="#666" />
+            <Icon name="eye-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponivel</Title>
+            <Description>R$ 197.332,12</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Compra mais recente em Indigo Estacionamentos no valor de R$ 6,00
+              hoje
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
